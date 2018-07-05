a9231d822602589f0d8e426db8fe02f14bab3216
Scott Hazelhurst <Scott.Hazelhurst@wits.ac.za>
2018-07-05 18:27:04 +0200

    Thurs eve

---
+++ src/genesisprototype/controller/Open0Controller.java
@@ -0,0 +1,93 @@
+package genesisprototype.controller;
+
+
+import genesisprototype.GenesisPrototype;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+/*
+ * Copyright (C) 2018 scott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author scott
+ */
+public class Open0Controller implements Initializable {
+    
+     
+    @FXML
+    private MenuBar menuBar;
+    
+    @FXML
+    private void newPCA(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(GenesisPrototype.class.getResource("view/PCADialogEntry.fxml"));
+        DialogPane page = (DialogPane) loader.load();
+        
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("New PCA details");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        Window primaryStage = menuBar.getScene().getWindow();
+        dialogStage.initOwner(primaryStage);
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+        PCADetailsController controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+        dialogStage.showAndWait();
+        
+        
+    }
+
+    
+    @FXML
+    private void handleKeyInput(final InputEvent event) {
+       if (event instanceof KeyEvent) {
+        final KeyEvent keyEvent = (KeyEvent) event;
+        if (keyEvent.isControlDown() && (keyEvent.getKeyCode() == KeyCode.A.getCode()) ){
+           provideAboutFunctionality();
+        }
+     }
+    }
+  
+    
+    private void provideAboutFunctionality() {
+        
+    }
+  
+    @Override
+    public void initialize(java.net.URL arg0, ResourceBundle arg1) {
+     menuBar.setFocusTraversable(true);
+   
+    }   
+    
+}
---
+++ src/genesisprototype/controller/PCADetailsController.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2018 scott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package genesisprototype.controller;
+
+/**
+ *
+ * @author scott
+ */
+ 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+/**
+ * Dialog to edit details of a person.
+ * 
+ * @author Marco Jakob
+ */
+public class PCADetailsController {
+
+    
+
+
+    private Stage dialogStage;
+    private boolean okClicked = false;
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+    /**
+     * Sets the stage of this dialog.
+     * 
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    
+    /**
+     * Returns true if the user clicked OK, false otherwise.
+     * 
+     * @return
+     */
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    /**
+     * Called when the user clicks ok.
+     */
+    @FXML
+    private void handleOk() {
+
+            okClicked = true;
+            dialogStage.close();
+       
+    }
+
+    /**
+     * Called when the user clicks cancel.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+    /**
+     * Validates the user input in the text fields.
+     * 
+     * @return true if the input is valid
+     */
+  
+}     
